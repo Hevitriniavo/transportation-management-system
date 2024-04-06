c7fcc5f57b186fc915b45c9be503c020be65c3fb
Hevitriniavo <hei.tantely@gmail.com>
2024-04-06 12:58:41 +0545

    feat: initializ project

---
+++ TransportationManagementSystem/src/test/java/hei/shool/transportationmanagementsystem/TransportationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.shool.transportationmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+<center>
+  <h1>Spring Boot + nextjs</h1>
+</center>
+<img src="./images/maxresdefault.jpg" alt="transport image">
+
